a5037310a783993eaf81b6b583ae507c953b27cc
João Cândido Messias <159557724+Joao-candido-messias@users.noreply.github.com>
2026-03-25 00:20:22 +0000

    Feat: Implementa a função sacar proposta no exercicio n° 1 da lista de exercicios proposta na aula4.

--- Aula4/ListaDeExercicios/ContaBancaria/ContaBancaria.java
+++ Aula4/ListaDeExercicios/ContaBancaria/ContaBancaria.java
@@ -1,6 +1,7 @@
 package Aula4.ListaDeExercicios.ContaBancaria;
 
 import java.util.Random;
+import java.util.Scanner;
 
 
 public class ContaBancaria {
@@ -9,17 +10,14 @@ public class ContaBancaria {
     private Double saldo;
     private Double limite;
 
-    public ContaBancaria(int numeroDaconta, Double saldo, Double limite){
-        this.numeroDaconta = numeroDaconta;
+    public ContaBancaria(Double saldo, Double limite){
+        Random rand = new Random();
+        this.numeroDaconta = rand.nextInt(9000) + 1000;
         this.saldo = saldo;
         this.limite = limite;
     }
 
-    //Setter de numero da conta
-    public void SetNumeroDaConta(){
-        Random rand = new Random();
-        this.numeroDaconta = rand.nextInt(9000) + 1000;
-    }
+
     //Getter de numero da conta
     public int GetNumeroDaConta(){
         return this.numeroDaconta;
@@ -46,5 +44,19 @@ public class ContaBancaria {
     //Getter de limite
     public Double Getlimite(){
         return this.limite;
+    }
+
+    public String sacar(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Quanto voce deseja sacar?");
+        double valor = scanner.nextDouble(); 
+        if(this.saldo < valor){
+            return "Saldo insuficiente.";
+        }
+        else{
+            this.saldo -= valor;
+            return "Saque efetuado com sucesso! Saldo atual da conta: "+this.saldo+;
+        }   
     }
 }
--- Aula4/ListaDeExercicios/ContaBancaria/Principal.java
+++ Aula4/ListaDeExercicios/ContaBancaria/Principal.java
@@ -2,6 +2,14 @@ package Aula4.ListaDeExercicios.ContaBancaria;
 
 public class Principal {
     public static void main(String[] args) {
-        
+
+        // Criando uma conta de teste
+        ContaBancaria conta = new ContaBancaria(1000.0, 500.0);
+
+        // Chamando o método sacar
+        String resultado = conta.sacar();
+
+        // Exibindo o resultado
+        System.out.println(resultado);
     }
 }
