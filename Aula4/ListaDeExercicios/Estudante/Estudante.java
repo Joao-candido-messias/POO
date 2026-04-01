960339b926d7b5603f2ff034d1e9045e1582d859
João Cândido Messias <159557724+Joao-candido-messias@users.noreply.github.com>
2026-04-01 02:21:07 +0000

    Fix: Corrige o return do exercicio 2.

--- Aula4/ListaDeExercicios/Estudante/Estudante.java
+++ Aula4/ListaDeExercicios/Estudante/Estudante.java
@@ -1,10 +1,17 @@
 package Aula4.ListaDeExercicios.Estudante;
+
+import java.util.Scanner;
     public class Estudante {
+        Scanner scanner = new Scanner(System.in);
         private double nota;
 
-        public void setNota(double nota) {
+        public void setNota() {
+            System.out.println("Inira a nota do aluno: ");
+            double nota = scanner.nextDouble();
             if (nota > 0 && nota < 10) {
                 this.nota = nota;
+            }else{
+                System.out.println("Nota inválida!");
             }
         }
         //Getter de nota
