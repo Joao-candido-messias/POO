51e1545aec787ecd60871f17876dc450760426ff
João Cândido Messias <159557724+Joao-candido-messias@users.noreply.github.com>
2026-04-01 02:38:48 +0000

    Implemta o exercicio 7(o sengundo 7) da lista proposta.

---
+++ Aula4/ListaDeExercicios/Termometro/Principal.java
@@ -0,0 +1,11 @@
+package Aula4.ListaDeExercicios.Termometro;
+
+public class Principal {
+    public static void main(String[] args) {
+        Termometro t = new Termometro();
+
+        t.setFahrenheit(68);
+        System.out.println(t.getCelsius());
+        System.out.println(t.getFahrenheit());
+    }
+}
---
+++ Aula4/ListaDeExercicios/Termometro/Termometro.java
@@ -0,0 +1,17 @@
+package Aula4.ListaDeExercicios.Termometro;
+
+public class Termometro {
+    private double celsius;
+
+    public double getFahrenheit() {
+        return (celsius * 9/5) + 32;
+    }
+
+    public void setFahrenheit(double f) {
+        this.celsius = (f - 32) * 5/9;
+    }
+
+    public double getCelsius() {
+        return celsius;
+    }
+}
