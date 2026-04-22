54c51fed76ea6f6fe76ed425a87d8dad7d6e0502
João Messias <joao.2020317758@aluno.iffar.edu.br>
2026-04-21 22:42:44 -0300

    Feat: Implementa o primeiro exercicio da lista de tratamento de exeções.

---
+++ Aula7/Calculadora/Calculadora.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Calculadora{
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+        int n1, n2, resultado;
+
+        try{
+
+            System.out.println("Insira o primeiro numero:");
+            n1 = scanner.nextInt();
+
+            System.out.println("Insira o segundo numero:");
+            n2 = scanner.nextInt();
+
+            resultado = n1/n2;
+
+            System.out.printf("Resultado: %d", resultado);
+
+        }catch(ArithmeticException e){
+            System.out.println("Erro matemático: impossível dividir por zero!");
+        }
+
+        scanner.close();
+        }
+}
