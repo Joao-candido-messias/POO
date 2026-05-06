26bbe1c99a4fe7fe77855cbea4c4310b822d11c8
João Cândido Messias <159557724+Joao-candido-messias@users.noreply.github.com>
2026-05-06 02:14:33 +0000

    chore: ajusta compilação e execução das classes Principal

--- Aula10/Exercicios/01/Principal.java
+++ Aula10/Exercicios/01/Principal.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class Principal {
+public class Principal{
     public static void main(String[] args) {
 
         ArrayList<NaveEspacial> frota = new ArrayList<>();
